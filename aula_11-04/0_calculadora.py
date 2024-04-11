83871bf2b19fd40e327a6e8524b6de579ea58387
Valdenei Xavier <valdenei.etech@gmail.com>
2024-04-11 18:49:15 -0400

    Update 0_calculadora.py
    adicionando condição while true(-1) e ifs dos operadores, convertendo as variaveis em float.

--- aula_11-04/0_calculadora.py
+++ aula_11-04/0_calculadora.py
@@ -4,14 +4,34 @@
 # seu programa deverá imprimir na tela o resultado da operação
 # Faça com que o seu programa funcione até que o usuário digite -1
 
-valor1 = input()
-operador = input()
-valor2 = input()
+while True:
+    valor1 = input("Digite um número (ou -1 para sair): ")
+    if valor1 == '-1':
+        print("Programa encerrado.")
+        break
+    
+    operador = input("Digite o operador (+, -, *, /): ")
+    valor2 = input("Digite outro número: ")
 
-resultado = 0
-if operador == '+':
-    resultado = valor1 + valor2
-else:
-    print('operador inválido')
+  
+    valor1 = float(valor1)
+    valor2 = float(valor2)
 
-print(resultado)
+    resultado = 0
+    if operador == '+':
+        resultado = valor1 + valor2
+    elif operador == '-':
+        resultado = valor1 - valor2
+    elif operador == '*':
+        resultado = valor1 * valor2
+    elif operador == '/':
+        if valor2 != 0: 
+            resultado = valor1 / valor2
+        else:
+            print("Não é possível dividir por zero.")
+            continue  
+    else:
+        print('Operador inválido')
+        continue 
+
+    print("Resultado:", resultado)
