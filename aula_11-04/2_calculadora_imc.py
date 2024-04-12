0c6b82ff7b5dfde1cf5b1ac1f520a665096f5d84
Valdenei Xavier <valdenei.etech@gmail.com>
2024-04-11 20:09:36 -0400

    Update 2_calculadora_imc.py
    adicionado mais condições de classificação do imc

--- aula_11-04/2_calculadora_imc.py
+++ aula_11-04/2_calculadora_imc.py
@@ -4,6 +4,7 @@
 # Seu programa deve calcular e imprimir o IMC do usuário seguindo a fórmula IMC = peso / (altura ** 2).
 # Além disso, deve classificar o resultado em: Baixo peso, Peso normal, Sobrepeso ou Obesidade, 
 # baseando-se nos valores de IMC.
+#fonte imc https://ge.globo.com/eu-atleta/nutricao/post/2022/08/17/veja-qual-e-o-imc-ideal-e-como-calcular.ghtml
 
 peso = float(input("Digite seu peso em kg: "))
 altura = float(input("Digite sua altura em metros: "))
@@ -13,9 +14,15 @@ imc = peso/(altura*altura)  # Cálculo do IMC precisa ser corrigido
 # Classificação do IMC (faltam informações)
 if imc < 18.5:
     classificacao = "Baixo peso"
-elif imc < 30:
-    classificacao = "Sobrepeso"
+elif imc < 24.9:
+    classificacao = "Peso Normal"
+elif imc< 29.9:
+    classificacao = "Sobrepeso"    
+elif imc< 34.9:
+    classificacao = "Obesidade grau 1"
+elif imc < 39.9:
+    classificacao = "Obesidade grau 2"            
 else:
-    classificacao = "Obesidade"
+    classificacao = "Obesidade Extrema"
 
 print(f"Seu IMC é {imc} e sua classificação é: {classificacao}")
